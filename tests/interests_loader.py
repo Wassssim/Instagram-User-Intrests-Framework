1b183fe8cdc79642044420cbf2db60517ed78dae
tawfik <tboujeh@gmail.com>
2020-03-21 20:20:42 +0100

    added needed interests in a docx file

---
+++ tests/interests_loader.py
@@ -0,0 +1,3 @@
+main_interests=("Buisness And Industry" , "Entertainment" , "Family and relationships"
+                , "Fitness And Wellness" , "Food And Drink" , "Hobbies And Activities" ,
+                "Shopping And Fashion" , "Sports And Outdoors" )
