40f2a3dd401565ac971c6e1d530163dafe73afcf
Wassssim <38323023+Wassssim@users.noreply.github.com>
2020-05-17 23:56:45 +0100

    code cleaner

--- dataset/collected_data/retrive_data.py
+++ dataset/collected_data/retrive_data.py
@@ -1,23 +1,23 @@
 import json
 import pandas as pd
 import hashtags as ht
-filename = 'technology.csv'
+import re
+filename = 'Entertainment.csv'
 
-def retrive_data(filename):
+def retrieve_data(filename):
     try:
         df=pd.read_csv(filename)
         return df
     except Exception as e:
             print(e)
 
-def remove_hashtags():
-    pass
-
 def clean_data():
-    df=retrive_data(filename)
     #print(df.captions.apply(ht.remove_hashtags(lambda x : remove_hashtags(str(x)))))
-    df.captions.str.findall(r'#.*?(?=\s|$)')
+    df=retrieve_data(filename)
+    df['captions'] = df['captions'].apply(lambda x: ht.remove_hashtags(x))
+    df.to_csv(filename)
 
 if __name__=="__main__":
     clean_data()
+    print("all good")
     
