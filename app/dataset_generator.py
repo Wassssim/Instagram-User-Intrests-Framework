81730323297348d31da5ce823422ce5ca72f822c
Wassssim <38323023+Wassssim@users.noreply.github.com>
2020-05-17 20:56:17 +0100

    dataset format updated to csv

--- app/dataset_generator.py
+++ app/dataset_generator.py
@@ -5,7 +5,7 @@ import json
 from os import listdir
 from os.path import isfile, join
 from time import sleep
-
+import pandas as pd
 
 
 
@@ -13,9 +13,9 @@ from time import sleep
 account_names_path="../dataset/account_names/"
 output_path="../dataset/collected_data/"
 instagram = Instagram()
-dataset = {}
-threshhold = 2000
-
+threshhold = 3
+columns = {"photo_url": 0, "captions": 1, "hashtags": 2, "interest": 3}
+data = []
 
 def get_all_files(my_path=account_names_path):
     onlyfiles = [f for f in listdir(my_path) if isfile(join(my_path, f))]
@@ -51,45 +51,46 @@ def generateDataset(input_filename):
     print ('scrapping...')
     interest = input_filename.replace(account_names_path,'')
     for line in Lines:
-        account = line[:-1].split(' ')[0]
-            
+        account = line[:-1].split(' ')[0]  
+        post = []
         medias=get_medias(account,threshhold)
         #______________________ We got Medias
-        if medias!=None :
-            user = {}
-            user['interest'] = interest
+        if medias!=None:
             ## Logging account 
             print("interest : "+interest+"\n")
             print("account name : "+account+"\n")
             for i in range(len(medias)):
                 #Filling Dict
-                user['post'+str(i)] = {}
-                user['post'+str(i)]['photo_url'] = medias[i].image_high_resolution_url #high_res
+                post = [0 for i in columns.keys()]
+                post[columns['interest']] = str(interest).lower()
+                post[columns['photo_url']] = str(medias[i].image_high_resolution_url) #high_res
                 caption = None
                 caption = deEmojify(medias[i].caption)
-                user['post'+str(i)]['caption'] = remove_hash_tags(caption)
-                user['post'+str(i)]['hashtags'] = extract_hash_tags(caption)
-                user['post'+str(i)]['comments'] = medias[i].comments_count
+                post[columns['captions']] = remove_hash_tags(caption)
+                post[columns['hashtags']] = extract_hash_tags(caption)
+                #user['post'+str(i)]['comments'] = medias[i].comments_count
                 ## Logging Post Number 
                 print("post :"+str(i))
-            dataset[account] = user
+                data.append(post)
+            #dataset[account] = user
     file.close()
     return interest
 
 if __name__=="__main__":
     #input_files = get_all_files( )
-    input_files=get_all_files()
+    input_files=["technology"]
     for input_file in input_files :
         input_file_added_to_path=account_names_path+input_file
         print(input_file_added_to_path )
         try:
             interest=generateDataset(input_file_added_to_path)
+            df = pd.DataFrame(data=data, columns=columns)
             print("Dataset generated")
-            output_file=input_file+".json"
+            print(df.info())
+            output_file=input_file+".csv"
             output_file_added_to_path=output_path+output_file
             with open(output_file_added_to_path, 'w', encoding='utf-8') as f:
-                json.dump(dataset, f, indent = 4)
-            dataset={}
+                df.to_csv(output_file_added_to_path)
         except Exception as e:
             print(e)
 
