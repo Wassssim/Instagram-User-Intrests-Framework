829eb8f67413866298c624365fa540eed2a6d989
Wassssim <38323023+Wassssim@users.noreply.github.com>
2020-05-17 21:55:15 +0100

    hashtags removal update

--- dataset/hashtags.py
+++ dataset/hashtags.py
@@ -6,10 +6,11 @@ def extract_hash_tags(s):
     return list(set(part[1:] for part in s.split() if part.startswith('#')))
 
 def remove_hash_tags(s):
+    s = re.sub("([^A-Za-z0-9]+#|^#)"," #",s)
     return ' '.join(re.sub(" #[A-Za-z0-9_]+"," ",s).split())
 
 
-test = "qdqsd #xDDD #xdqsd_qsdqs aeaze#123eaze"
+test = "#qdqsd #xDDD #xdqsd_qsdqs aeaze.#123eaze"
 
 print("Hashtags only: "+str(extract_hash_tags(test)))
 print("Without hashtags: "+remove_hash_tags(test))
