4f43f310dff36762cf52b51d9ced20f95f5e8592
tawfik <tboujeh@gmail.com>
2020-05-22 21:45:13 +0200

    Final data

--- dataset/collected_data/data_cleaner.py
+++ dataset/collected_data/data_cleaner.py
@@ -4,8 +4,26 @@ import hashtags as ht
 import re
 interest = 'technology'
 filename=interest+".csv"
-def merge():
-    pass
+def get_filename(interest):
+    filename=interest+".csv"
+    return filename
+
+def output_interest(interest_1,interest_2):
+    output=interest_1+" and "+interest_2
+    return output
+
+def merge(interest_1,interest_2):
+    filename_1=get_filename(interest_1)
+    filename_2=get_filename(interest_2)
+    df1=retrieve_data(filename_1)
+    df2=retrieve_data(filename_2)
+    df = df1.append(df2, ignore_index = True)
+    my_interest= output_interest(interest_1,interest_2)    
+    df['interest']=my_interest
+    saving_file=get_filename(my_interest)
+    save_data(df,saving_file)
+    print(df)
+
 def retrieve_data(filename):
     try:
         df=pd.read_csv(filename)
@@ -21,11 +39,16 @@ def clean_data(df):
     #print(df.head())
     return df
 
-
-if __name__=="__main__":
-    data=retrieve_data(filename)
-    clean_data=clean_data(data)
-    print(clean_data.head())
+def save_data(clean_data,filename):
     clean_data.to_csv(filename)    
+    
+if __name__=="__main__":
+    #data=retrieve_data(filename)
+    #clean_data=clean_data(data)
+    #print(clean_data.head())
+    #clean_data.to_csv(filename)    
+    interest_1="Business"
+    interest_2="Industry"
+    merge(interest_1,interest_2)
     print("all good")
     
